0b77e8021f5d5713ef3604b002366fd2099c1274
vladislavsumin <vladislavsumin@mail.ru>
2019-12-24 22:50:49 +0300

    add load properties task

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,24 @@
-rootProject.name="MyHomeIot"
+import java.util.Properties
+import java.io.File
+
+rootProject.name = "MyHomeIot"
 include(":app")
+injectLocalProperties("local.properties")
+
+
+/**
+ * Load properties from file and add/override properties for all project
+ *
+ * @param propertiesPath - path to properties
+ */
+fun injectLocalProperties(propertiesPath: String, skipIfNotExist: Boolean = true) {
+    //TODO add logs
+    val file = File(propertiesPath)
+    if (skipIfNotExist && !file.exists()) return
+    val reader = file.reader()
+    val properties = Properties()
+    reader.use { properties.load(it) }
+    gradle.beforeProject {
+        properties.stringPropertyNames().forEach { this.extra.set(it, properties[it]) }
+    }
+}
