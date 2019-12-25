878427e668ccff6f553ba471d5453eeb418cefeb
vladislavsumin <vladislavsumin@mail.ru>
2019-12-25 21:36:49 +0300

    add firebase disable options and shrink resources for release

--- app/src/main/java/ru/vladislavsumin/myhomeiot/app/AppConfig.kt
+++ app/src/main/java/ru/vladislavsumin/myhomeiot/app/AppConfig.kt
@@ -3,5 +3,6 @@ package ru.vladislavsumin.myhomeiot.app
 import ru.vladislavsumin.myhomeiot.BuildConfig
 
 object AppConfig {
-    public val VERSION_NAME = BuildConfig.VERSION_NAME
+    const val VERSION_NAME = BuildConfig.VERSION_NAME
+    const val ENABLE_FIREBASE = BuildConfig.ENABLE_FIREBASE
 }
